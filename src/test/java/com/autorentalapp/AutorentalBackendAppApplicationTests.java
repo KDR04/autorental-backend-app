048e4d945acc727ae8db643a309d9dfb4be09a65
KDR04 <kaushik.dt2@gmail.com>
2021-04-26 16:41:39 +0530

    initial commit

---
+++ src/test/java/com/autorentalapp/AutorentalBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autorentalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutorentalBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
